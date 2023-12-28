1ba2e63057a61f84725c417ce6cba2e4affc5a1e
Anthony <antc615@gmail.com>
2023-12-28 14:51:16 -0600

    updates for splash screen

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
-
-import {View, Text, Image, TouchableOpacity} from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 import logo from '../assets/gllogo1.png';
-
-import {styles} from '../styles/SplashScreen.styles';
-
+import { styles } from '../styles/SplashScreen.styles';
 import LinearGradient from 'react-native-linear-gradient';
 
 const SplashScreen: React.FC = () => {
@@ -12,9 +9,15 @@ const SplashScreen: React.FC = () => {
     <LinearGradient colors={['#ff8c00', '#ff0080']} style={styles.background}>
       <View style={styles.container}>
         <Image source={logo} style={styles.logo} />
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Register</Text>
-        </TouchableOpacity>
+        <Text style={styles.titleText}>GoLucky</Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Register</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button}>
+            <Text style={styles.buttonText}>Log In</Text>
+          </TouchableOpacity>
+        </View>
         <Text style={styles.footerText}>Online Dating App</Text>
       </View>
     </LinearGradient>
--- src/styles/SplashScreen.styles.ts
+++ src/styles/SplashScreen.styles.ts
@@ -1,24 +1,40 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 
 export const styles = StyleSheet.create({
   background: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start', // Changed to flex-start to move content towards the top
     alignItems: 'center',
   },
   container: {
+    flex: 1,
+    justifyContent: 'space-between', // Adjusted for spacing
     alignItems: 'center',
+    paddingTop: 60, // Added padding at the top
+    paddingBottom: 40, // Added padding at the bottom
   },
   logo: {
     width: 150,
     height: 150,
     resizeMode: 'contain',
   },
+  titleText: {
+    fontSize: 24,
+    color: 'white',
+    fontWeight: 'bold',
+    // fontFamily: 'YourCustomFont', // Include this if you have a custom font.
+    marginTop: 20, // Space between logo and title
+  },
+  buttonContainer: {
+    width: '100%',
+    alignItems: 'center',
+  },
   button: {
     backgroundColor: 'white',
     paddingHorizontal: 30,
     paddingVertical: 10,
     borderRadius: 20,
+    marginTop: 15, // Space between buttons
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
@@ -31,12 +47,11 @@ export const styles = StyleSheet.create({
   buttonText: {
     fontSize: 18,
     color: '#000',
-    // fontFamily: 'YourCustomFont', // Make sure to include your custom font if you have one.
+    // fontFamily: 'YourCustomFont',
   },
   footerText: {
     fontSize: 16,
     color: '#fff',
-    marginTop: 20,
-    // fontFamily: 'YourCustomFont', // Include this if you have a custom font.
+    // fontFamily: 'YourCustomFont',
   },
 });
