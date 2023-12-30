a2cb40db5f85aa666ec55cb8b3742ba6cefa986b
Anthony <antc615@gmail.com>
2023-12-30 00:16:24 -0600

    minor styling

--- src/styles/Messages.styles.ts
+++ src/styles/Messages.styles.ts
@@ -4,29 +4,38 @@ import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
   container: {
-    // Container styles (if needed)
+    flex: 1,
+    backgroundColor: '#FFF', // Consistent background color
   },
   messageBlock: {
     flexDirection: 'row',
+    backgroundColor: '#f7f7f7', // Light grey background for each message block
     padding: 10,
-    // Add more styles for the message block
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3, // for Android
+    marginBottom: 10,
+    alignItems: 'center', // Align items in the center vertically
   },
   image: {
-    width: 50,
-    height: 50,
-    borderRadius: 25,
-    // Additional styles for the image
+    width: 60, // Adjust as needed
+    height: 60,
+    borderRadius: 30,
+    marginRight: 10,
   },
   textContainer: {
     flex: 1,
-    marginLeft: 10,
-    // Adjust as needed
   },
   username: {
     fontWeight: 'bold',
-    // Username styles
+    fontSize: 16,
+    color: '#E91E63', // Theme color for username
   },
   description: {
-    // Description text styles
+    color: '#666', // Subtle color for description
+    fontSize: 14,
   },
 });
