0ba7e2877c6c9a129405baaed48c3fcb0e6f877b
Anthony <antc615@gmail.com>
2023-12-30 00:11:54 -0600

    enhance profile

--- src/styles/Profile.styles.ts
+++ src/styles/Profile.styles.ts
@@ -11,7 +11,6 @@ export const styles = StyleSheet.create({
     flexDirection: 'row',
     flexWrap: 'wrap',
     justifyContent: 'space-between',
-    marginBottom: 20,
   },
   imagePlaceholder: {
     width: '30%',
@@ -24,12 +23,25 @@ export const styles = StyleSheet.create({
     marginBottom: 10,
     backgroundColor: '#f0f0f0', // A light background color
   },
+  photoUploadInstructions: {
+    textAlign: 'center',
+    marginVertical: 10,
+  },
   section: {
-    marginBottom: 20,
+    backgroundColor: '#FFF', // Light background for each section
+    borderRadius: 8,
+    padding: 15,
+    marginBottom: 10,
+    shadowColor: '#000',
+    shadowOffset: {width: 0, height: 2},
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3, // for Android
   },
   sectionTitle: {
-    fontSize: 18,
+    fontSize: 20,
     fontWeight: 'bold',
+    color: '#E91E63', // Theme color for section titles
     marginBottom: 10,
   },
   input: {
@@ -38,6 +50,35 @@ export const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 10,
     marginBottom: 10,
+    backgroundColor: '#f7f7f7', // Light grey background for input fields
   },
-  // Add more styles as needed
+  tagContainer: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginBottom: 10,
+  },
+  tag: {
+    backgroundColor: '#FFC107', // A warm color for tags
+    borderRadius: 20,
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    marginRight: 5,
+    marginBottom: 5,
+  },
+  tagText: {
+    color: '#FFF', // White text for tags
+    fontSize: 14,
+  },
+  saveButton: {
+    backgroundColor: '#4CAF50', // Green color for the save button
+    padding: 15,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 20,
+  },
+  saveButtonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
 });
